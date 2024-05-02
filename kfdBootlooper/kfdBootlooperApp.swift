24dd6281494276f5c7f10cd5b5ceccdfeff9087a
Skadz <skadz@skadz.online>
2024-05-01 22:07:40 -0700

    Initial Commit

---
+++ kfdBootlooper/kfdBootlooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  kfdBootlooperApp.swift
+//  kfdBootlooper
+//
+//  Created by Skadz on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct kfdBootlooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
